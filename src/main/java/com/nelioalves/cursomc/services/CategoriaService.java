550819e8ff2ea4fa864a2d4432e5ab62c690ee27
renan <xnooneyouthink@gmail.com>
2018-12-17 00:08:59 -0200

    .

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,14 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElse(null);
+	}
+	
+	public Categoria save(Categoria categoria) {
+		return repo.save(categoria);
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 
 }
